f91acf9a410edf394369766534955651d6373fb0
takahiro.tominaga <tt.300607@gmail.com>
2022-04-11 23:39:07 +0900

    feat: create a new project (#1)

---
+++ SimpleToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleToDoApp.swift
+//  SimpleToDo
+//
+//  Created by Takahiro Tominaga on 2022/04/11.
+//
+
+import SwiftUI
+
+@main
+struct SimpleToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
