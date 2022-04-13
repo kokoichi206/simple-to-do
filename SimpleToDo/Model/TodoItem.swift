f9763f74eaa6422015e51c3004b6c412a5086046
takahiro.tominaga <tt.300607@gmail.com>
2022-04-13 21:06:21 +0900

    add: todo model (#6)

---
+++ SimpleToDo/Model/TodoItem.swift
@@ -0,0 +1,32 @@
+//
+//  TodoItem.swift
+//  SimpleToDo
+//
+//  Created by Takahiro Tominaga on 2022/04/13.
+//
+
+import Foundation
+
+/*
+ Todo1つ分のモデル
+ */
+struct TodoItem: Identifiable {
+
+    let id: String
+    let title: String
+    let done: Bool
+
+    init(id: String = UUID().uuidString, title: String, done: Bool) {
+        self.id = id
+        self.title = title
+        self.done    = done
+    }
+
+    func updateTitle(newTitle: String) -> TodoItem {
+        return TodoItem(id: id, title: newTitle, done: done)
+    }
+
+    func toggleDone() -> TodoItem {
+        return TodoItem(id: id, title: title, done: !done)
+    }
+}
